771ed0730555aca708db20d7f0c7b073cb77edb5
visualstudio <visualstudio@122-PC17>
2019-01-18 21:10:31 -0500

    Commit1

--- c0730319_arrays/Program.cs
+++ c0730319_arrays/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,31 @@ namespace c0730319_arrays
     {
         static void Main(string[] args)
         {
+            studentinformation _mary = new studentinformation("mary", 3.8, 20);
+            studentinformation _john = new studentinformation("john", 3.8, 10);
+            studentinformation _mark = new studentinformation("mark", 0.2, 3);
+            studentinformation _steve = new studentinformation("steve", 2.9, 16);
+            studentinformation _cindy = new studentinformation("cindy", 3.9, 18);
+            Console.ReadLine();
+        
+    }
+
+        class studentinformation
+        {
+            public string name;
+            public double gpa;
+            public int attendance;
+
+            public studentinformation(string aname, double cgpa, int dattendance)
+            {
+
+            name = aname;
+            gpa = cgpa;
+            attendance =dattendance;
+
+            }
         }
     }
+    
 }
+
